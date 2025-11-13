19529760a5af499c22c8d9c1a75072e38a5041bd
himanshupatel2007 <himanshupatel.942007@gmail.com>
2025-11-13 12:45:36 +0530

    Volume Program added

---
+++ volume.py
@@ -0,0 +1,5 @@
+length = float(input("Enter the Length : "))
+width = float(input("Enter the Widht : "))
+hieght = float(input("enter the Hieght : "))
+volume = hieght * width * length 
+print(f"Volume : {volume}")
