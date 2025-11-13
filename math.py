25fc75b2e94bc040e95844ee5b46f8793ab973ec
himanshupatel2007 <himanshupatel.942007@gmail.com>
2025-11-13 21:42:16 +0530

    Math function understod

---
+++ math.py
@@ -0,0 +1,8 @@
+x = 3.14
+y = -4
+z= 13
+print(f"round of {x} is {round(x)}")
+print(f"Absolute value of {y} is {abs(y)}")
+print(f"square of {z} is {pow(z,2)}")
+print(f"maximum of {x} {y} {z} is {max(x,y,z)}")
+print(f"minimum of {x} {y} {z} is {min(x,y,z)}")
